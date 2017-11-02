173feba546efadb9f68bdf0b782e5d6289662f8a
cbabalis <babisbabalis@gmail.com>
2017-11-02 18:32:12 +0200

    convert to class

--- network_operations/net_ops.py
+++ network_operations/net_ops.py
@@ -9,28 +9,34 @@ import arcpy
 import sys
 
 
-def get_point_coordinates():
-    """ This method acquires the coordinates of a point in the map.
+class Network_Operations:
+    """ This class implements basic network operations.
     """
-    pass
+    def __init__(self):
+        pass
 
-def compute_distance_between(point_a, point_b):
-    """ This method computes the distance between two points.
-    """
-    pass
+    def get_point_coordinates():
+        """ This method acquires the coordinates of a point in the map.
+        """
+        pass
 
-def insert_node():
-    """ This method inserts a new node in the network.
-    """
-    pass
+    def compute_distance_between(point_a, point_b):
+        """ This method computes the distance between two points.
+        """
+        pass
 
-def delete_node():
-    """ This method deletes a node from the network.
-    """
-    pass
+    def insert_node():
+        """ This method inserts a new node in the network.
+        """
+        pass
 
-def is_network_robust():
-    """ This method checks the continuity of the network.
-    :return : True if the network is continuous, False otherwise.
-    """
-    pass
+    def delete_node():
+        """ This method deletes a node from the network.
+        """
+        pass
+
+    def is_network_robust():
+        """ This method checks the continuity of the network.
+        :return : True if the network is continuous, False otherwise.
+        """
+        pass
