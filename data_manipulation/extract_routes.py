f74a82b5042bec57195c1dca7fafb296e39da66a
Babis Babalis <babisbabalis@gmail.com>
2017-12-04 20:25:06 +0200

    open the workbook code has been written

--- data_manipulation/extract_routes.py
+++ data_manipulation/extract_routes.py
@@ -5,20 +5,29 @@ writes it to a .csv file.
 In particular, two scenarios are being examined. Given a file of GPS points:
     - extract all data relevant to specific routes (i.e. Attiki Odos +/-1 stop)
     - extract all routes a truck has been done.
+
+    To run this module as an independent script, type:
+    > python extract_routes.py ~/my_folder output.csv
 """
 
 
 import pdb
 import xlrd
+import os
 import sys
 
 class Extraction:
     """ Extracts data from several files to a new one.
     """
 
-    def __init__(self, ):
-        self.file_contents = ""
-        pass
+    def __init__(self, src_folder, dest_file):
+        self.file_contents = []
+        # read the file
+        self.read_folder(src_folder)
+        # process data
+        self.process_file()
+        # and write data to a new file
+        self.write_output(dest_file)
 
     def read_folder(self, folder=""):
         """ This method reads a folder which contains files as input.
@@ -26,13 +35,28 @@ class Extraction:
         :param str folder: is the folder where the function looks
         for files recursively
         """
-        pass
+        # search recursively the system
+        for root, dirs, files in os.walk(folder):
+            for f in files:
+                if f.endswith(".xls"):
+                    # read the xls file
+                    xls_file = os.path.join(root, f)
+                    self._read_xls(xls_file)
 
-    def process_file(self, filename=""):
+    def _read_xls(self, xls_file):
+        """ This method reads a single xls file."""
+        # open the workbook
+        workbook = xlrd.open_workbook(xls_file)
+        # split all sheets by name
+        worksheets = workbook.sheet_names()
+        # acquire the first one
+        worksheet = workbook.sheet_by_name(worksheets[0])
+        # and append it to the list of files
+        self.file_contents.append(worksheet)
+
+    def process_file(self):
         """ This method processes a file and extracts only the data
         the user is interested for.
-
-        :param str filename: is the name of the file.
         """
         pass
 
@@ -42,4 +66,4 @@ class Extraction:
         :param str write_filename: the filename of the file to
         be written.
         """
-        pass""
+        pass
